2cd624dde286cfa5566803cc3d1a09156655a568
febrianide <febriani.dewi.alviah@students.untidar.ac.id>
2024-09-03 09:40:32 +0700

    Project pertemuan kedua

---
+++ Febb_project1.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.feb_project1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Feb_project1 {
+
+    public static void main(String[] args) {
+        // Menyimpan Informasi Pribadi
+        String nama = "Febriani Dewi Alviah";
+        String npm = "2310506010";
+        String alamat = "Pati";
+        String noHp = "08111222333";
+        
+        //Menampilkan Informasu Pribadi
+        System.out.println("Nama  :" + nama);
+        System.out.println("NPM  :" + npm);
+        System.out.println("Alamat  :" + alamat);
+        System.out.println("No Hp  :" + noHp);
+        
+   
+                
+    
+    }
+}
